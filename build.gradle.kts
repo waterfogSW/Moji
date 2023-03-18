677aef1f694a2f9553e5c3fc459212d871c60f17
waterfog9580 <waterfog9580@gmail.com>
2023-03-18 20:18:31 +0900

    [#7] :sparkles: create UserService

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,17 @@ project(":discovery-service") {
     }
 }
 
+project(":user-service") {
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+        // MySQL
+        runtimeOnly("com.mysql:mysql-connector-j")
+    }
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
