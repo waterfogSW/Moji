ace91f9b7e7bfafa67b9f524d4d79089290b8e65
waterfog9580 <waterfog9580@gmail.com>
2023-04-16 11:04:17 +0900

    [#9] :wrench: register eureka user-service

--- user-service/src/main/kotlin/com/waterfogsw/userservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/com/waterfogsw/userservice/UserServiceApplication.kt
@@ -2,8 +2,10 @@ package com.waterfogsw.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class UserServiceApplication
 
 fun main(args: Array<String>) {
