1b1f91539cf75c944e0bbe354db96c0f23e6195d
waterfog9580 <waterfog9580@gmail.com>
2023-04-18 21:03:55 +0900

    [#12] :fire: Remove DiscoveryService for k8s  adoption

--- user-service/src/main/kotlin/com/waterfogsw/userservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/com/waterfogsw/userservice/UserServiceApplication.kt
@@ -2,10 +2,8 @@ package com.waterfogsw.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class UserServiceApplication
 
 fun main(args: Array<String>) {
