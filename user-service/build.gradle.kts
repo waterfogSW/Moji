c11192ffda09505a2c7c78f1540698d187c67656
San Kim <waterfog9580@gmail.com>
2023-04-22 21:33:33 +0900

    [#18] :recycle: separate dependency manage

---
+++ user-service/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // MySQL
+    runtimeOnly("com.mysql:mysql-connector-j")
+}
